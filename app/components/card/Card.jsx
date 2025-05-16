fed93258ea0d1c49ea2d11d11adc675d57e1f165
Sadaf Shahab <sadafshahab123@yahoo.com>
2025-05-16 17:14:53 +0500

    continue post section

--- app/components/card/Card.jsx
+++ app/components/card/Card.jsx
@@ -1,25 +1,29 @@
 import React from "react";
 import styles from "./card.module.css";
 import Image from "next/image";
+import Link from "next/link";
 const Card = () => {
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
-        <Image src={"/post1.jpg"} alt="recent" fill />
+        <Image src={"/post1.jpg"} alt="recent" fill className={styles.image} />
       </div>
       <div className={styles.textContainer}>
-        <div className={styles.detail}>
-
+        <div className={styles.details}>
+          <span className={styles.date}>11.05.2025 - </span>
+          <span className={styles.category}>Category</span>
         </div>
-        <h1>A Global Symbol of Renewal</h1>
-        <p>
+        <Link href={"/"}>
+          <h1 className={styles.postTitle}>A Global Symbol of Renewal</h1>
+        </Link>
+        <p className={styles.desc}>
           Cherry blossoms, or sakura as they’re known in Japan, have captivated
           cultures for centuries. Originating in East Asia, they’ve become
           global icons of beauty, hope, and renewal. While Japan is undoubtedly
           the spiritual home of the cherry blossom, you can now find these trees
           blooming from Washington, D.C. to Paris, Vancouver, and Seoul.
         </p>
-        <button>Read More</button>
+        <Link href={"/"} className={styles.postLink}>Read More</Link>
       </div>
     </div>
   );
--- app/components/menu/Menu.tsx
+++ app/components/menu/Menu.tsx
@@ -1,11 +1,94 @@
-import React from 'react';
-import styles from "./menu.module.css"
+import React from "react";
+import styles from "./menu.module.css";
+import Link from "next/link";
+import Image from "next/image";
 const Menu = () => {
   return (
     <div className={styles.container}>
-      this is navbar
+      <h2 className={styles.subTitle}>{`What's`} hot</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+      <div className={styles.items}>
+        <Link href={"/"} className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={"/post1.jpg"}
+              alt="itemImages"
+              fill
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.travel}`}>
+              Travel
+            </span>
+            <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet.</h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>John Doe</span>
+              <span className={styles.date}> - 05.05.2025</span>
+            </div>
+          </div>
+        </Link>
+        <Link href={"/"} className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={"/post1.jpg"}
+              alt="itemImages"
+              fill
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.culture}`}>
+              Culture
+            </span>
+            <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet.</h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>John Doe</span>
+              <span className={styles.date}> - 05.05.2025</span>
+            </div>
+          </div>
+        </Link>
+        <Link href={"/"} className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={"/post1.jpg"}
+              alt="itemImages"
+              fill
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.food}`}>Food</span>
+            <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet.</h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>John Doe</span>
+              <span className={styles.date}> - 05.05.2025</span>
+            </div>
+          </div>
+        </Link>
+        <Link href={"/"} className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={"/post1.jpg"}
+              alt="itemImages"
+              fill
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.fashion}`}>
+              Fashion
+            </span>
+            <h3 className={styles.postTitle}>Lorem ipsum dolor sit amet.</h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>John Doe</span>
+              <span className={styles.date}> - 05.05.2025</span>
+            </div>
+          </div>
+        </Link>
+      </div>
     </div>
   );
-}
+};
 
 export default Menu;
