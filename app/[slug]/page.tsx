917ebd31d279636d6ce23e9f8e019de88543fa21
sadafshahab12 <sadafshahab123@yahoo.com>
2025-06-25 12:25:51 -0700

    create slug, worked on single page and now creating comment section

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styles from "./singlePage.module.css";
+import Menu from "../components/menu/Menu";
+import Image from "next/image";
+const SinglePage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </h1>
+          <div className={styles.user}>
+            <div className={styles.userImgContainer}>
+              <Image src="/cl3.jpg" alt="" fill className={styles.avatar} />
+            </div>
+            <div className={styles.userTextContainer}>
+              <span className={styles.username}>John Doe</span>
+              <span className={styles.date}>01 . 01 . 2025</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image src="/cl3.jpg" alt="" fill className={styles.image} />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.post}>
+          <div className={styles.description}>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci a possimus quis exercitationem tenetur expedita quidem sed maiores autem, magnam rem corporis quisquam est at suscipit rerum error
+              iusto alias aspernatur.
+            </p>
+            <h2 className={styles.descriptionTitle}>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci a possimus quis exercitationem tenetur expedita quidem sed maiores autem, magnam rem corporis quisquam est at suscipit rerum error
+              iusto alias aspernatur.
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci a possimus quis exercitationem tenetur expedita quidem sed maiores autem, magnam rem corporis quisquam est at suscipit rerum error
+              iusto alias aspernatur.
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci a possimus quis exercitationem tenetur expedita quidem sed maiores autem, magnam rem corporis quisquam est at suscipit rerum error
+              iusto alias aspernatur.
+            </p>
+          </div>
+        </div>
+        <Menu />
+      </div>
+    </div>
+  );
+};
+
+export default SinglePage;
---
+++ app/components/comments/Comments.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./comments.module.css";
+const Comments = () => {
+  return <div></div>;
+};
+
+export default Comments;
